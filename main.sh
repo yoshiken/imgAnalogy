888a25e3b35d39f5497950b864d14051ccf9f0b1
yskn <yskn.beer@gmail.com>
2018-01-28 19:06:36 +0900

    機能完成

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
+#!/bin/sh
 
-python main.py >Filename_Analogy
-sort Filename_Analogy > Filename_Analogy_sorted
-cat Filename_Analogy_sorted | cut -d' ' -f2 > Analogy_sorted
-python gurahu.py
+files="./imgdataset/*"
+for filepath in $files; do
+    echo $filepath
+    SECONDS=0
+    python main.py $filepath > Filename_Analogy
+    fname="${filepath%.*}"
+    fname="${fname#*/*/}"
+    sort Filename_Analogy > ./res/$fname
+    time=$SECONDS
+    echo $time
+done
+#cat Filename_Analogy_sorted | cut -d' www' -f2 > Analogy_sorted
